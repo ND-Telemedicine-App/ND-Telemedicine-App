84995d69c0addf2230dd003ad45412937869b8fd
doanyennhi <s3880599@rmit.edu.vn>
2022-08-29 17:33:19 +1000

    Remove green part from profile heading, add user info

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -11,29 +11,68 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: Color(0xffFDFFFE),
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(32),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16.0),
-                  color: Color(0xFF38B69A)
-                ),
-                // color: Color(0x38B69A),
-                child: Column(
-                  children: const [
-                    CircleAvatar(
-                      radius: 100,
-                      backgroundImage: AssetImage("assets/images/yeonjun.jpg"),
-                    ),
-                    Text("Choi Yeonjun")
-                  ]
+              // profile heading
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage("assets/images/yeonjun.jpg"),
               ),
+              Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 10),
+                child: Text("Choi Yeonjun",
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Color(0xff031011),
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              Text("No status",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xff031011),
+
+                  )),
+              // Info section
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 40),
+                padding: EdgeInsets.all(20),
+                width: screenWidth * 0.8,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color(0xffFDFFFE),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Male"),
+                          Text("cyj1309@gmail.com")
+                          ]
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("0413939449"),
+                          Text("13/09/1999")
+                        ]
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("123 Elizabeth St, Melbourne, VIC 3000")
+                        ]
+                    )
+                  ]
+              )
               )
           ]
         )
