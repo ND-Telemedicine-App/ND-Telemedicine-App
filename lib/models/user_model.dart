c2bc6457c1e5f14cabf29815a81df622923be814
Hoang Linh <hlinh.jobs@gmail.com>
2022-09-06 17:09:39 +1000

    finish basic UI Home screen

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -16,7 +16,7 @@ class User {
 final User currentUser = User(
   id: 0,
   name: 'Nick Fury',
-  imageUrl: 'assets/images/nick-fury.jpg',
+  imageUrl: 'assets/images/chats/nick-fury.jpg',
   isOnline: true,
 );
 
@@ -24,48 +24,48 @@ final User currentUser = User(
 final User ironMan = User(
   id: 1,
   name: 'Iron Man',
-  imageUrl: 'assets/images/ironman.jpeg',
+  imageUrl: 'assets/images/chats/ironman.jpeg',
   isOnline: true,
 );
 final User captainAmerica = User(
   id: 2,
   name: 'Captain America',
-  imageUrl: 'assets/images/captain-america.jpg',
+  imageUrl: 'assets/images/chats/captain-america.jpg',
   isOnline: true,
 );
 final User hulk = User(
   id: 3,
   name: 'Hulk',
-  imageUrl: 'assets/images/hulk.jpg',
+  imageUrl: 'assets/images/chats/hulk.jpg',
   isOnline: false,
 );
 final User scarletWitch = User(
   id: 4,
   name: 'Scarlet Witch',
-  imageUrl: 'assets/images/scarlet-witch.jpg',
+  imageUrl: 'assets/images/chats/scarlet-witch.jpg',
   isOnline: false,
 );
 final User spiderMan = User(
   id: 5,
   name: 'Spider Man',
-  imageUrl: 'assets/images/spiderman.jpg',
+  imageUrl: 'assets/images/chats/spiderman.jpg',
   isOnline: true,
 );
 final User blackWindow = User(
   id: 6,
   name: 'Black Widow',
-  imageUrl: 'assets/images/black-widow.jpg',
+  imageUrl: 'assets/images/chats/black-widow.jpg',
   isOnline: false,
 );
 final User thor = User(
   id: 7,
   name: 'Thor',
-  imageUrl: 'assets/images/thor.png',
+  imageUrl: 'assets/images/chats/thor.png',
   isOnline: false,
 );
 final User captainMarvel = User(
   id: 8,
   name: 'Captain Marvel',
-  imageUrl: 'assets/images/captain-marvel.jpg',
+  imageUrl: 'assets/images/chats/captain-marvel.jpg',
   isOnline: false,
 );
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nd_telemedicine_app/utils/category_field.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,172 +12,336 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          //  App Bar
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
+        body: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: SafeArea(
+        child: Column(
+          children: [
+            //  App Bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(right: 12),
+                          child: CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.transparent,
+                            backgroundImage:
+                                AssetImage("assets/images/kuri.jpeg"),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Good morning,",
+                            style: TextStyle(
+                                fontFamily: "PoppinsItalic", fontSize: 14),
+                          ),
+                          const SizedBox(
+                            height: 2,
+                          ),
+                          Text(
+                            "Username",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: "PoppinsSemiBold",
+                                color: Theme.of(context).primaryColor),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Icon(
+                          Icons.notifications,
+                          color: Colors.black,
+                          size: 28,
+                        ),
+                      ),
+                      Icon(Icons.message_rounded, color: Colors.black)
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    border: InputBorder.none,
+                    hintText: "How can we help you?",
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 5,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    )
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        "assets/images/home_med.png",
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "How do you feel today?",
+                            style: TextStyle(
+                                fontFamily: "PoppinsSemiBold",
+                                fontSize: 20,
+                                color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("Fill out your medical form!",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.white)),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Get started",
+                                style: TextStyle(
+                                    fontFamily: "PoppinsMedium",
+                                    fontSize: 15,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Text(
+                      "Doctor Speciality",
+                      style: TextStyle(fontFamily: "PoppinsBold", fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    child: TextButton(
+                      onPressed: () => {},
+                      child: Text(
+                        "See more",
+                        style: TextStyle(
+                            fontFamily: "PoppinsMedium",
+                            fontSize: 13,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CategoryField(
+                          iconImagePath: "assets/icons/general.png",
+                          categoryName: "General"),
+                      CategoryField(
+                          iconImagePath: "assets/icons/tooth.png",
+                          categoryName: "Dentist"),
+                      CategoryField(
+                          iconImagePath: "assets/icons/view.png",
+                          categoryName: "Ophthalmology"),
+                      CategoryField(
+                          iconImagePath: "assets/icons/surgery.png",
+                          categoryName: "Surgery"),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CategoryField(
+                          iconImagePath: "assets/icons/brain.png",
+                          categoryName: "Neurology"),
+                      CategoryField(
+                          iconImagePath: "assets/icons/children.png",
+                          categoryName: "Pediatric"),
+                      CategoryField(
+                          iconImagePath: "assets/icons/cardio.png",
+                          categoryName: "Cardiology"),
+                      CategoryField(
+                          iconImagePath: "assets/icons/diet.png",
+                          categoryName: "Nutrition"),
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Text(
+                      "Doctors",
+                      style: TextStyle(fontFamily: "PoppinsBold", fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    child: TextButton(
+                      onPressed: () => {},
+                      child: Text(
+                        "See more",
+                        style: TextStyle(
+                            fontFamily: "PoppinsMedium",
+                            fontSize: 13,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: EdgeInsets.all(15),
+                width: MediaQuery.of(context).size.width - 25.0 * 2,
+                height: 150,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey[200]),
+                child: Row(
                   children: [
                     Container(
-                        margin: EdgeInsets.only(right: 12),
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Colors.transparent,
-                          backgroundImage:
-                              AssetImage("assets/images/kuri.jpeg"),
-                        )),
+                      padding: EdgeInsets.only(right: 20),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          "assets/images/doctor2.jpeg",
+                          height: 120,
+                          width: 120,
+                        ),
+                      ),
+                    ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text(
-                          "Good morning,",
-                          style: TextStyle(
-                              fontFamily: "PoppinsItalic", fontSize: 14),
-                        ),
-                        const SizedBox(
-                          height: 2,
-                        ),
-                        Text(
-                          "Username",
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: "PoppinsSemiBold",
-                              color: Theme.of(context).primaryColor),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 15),
-                      child: Icon(
-                        Icons.notifications,
-                        color: Colors.black,
-                        size: 28,
-                      ),
-                    ),
-                    Icon(Icons.message_rounded, color: Colors.black)
-                  ],
-                )
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Container(
-              padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(12)),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Image.asset(
-                      "assets/images/home_med.png",
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "How do you feel today?",
-                          style: TextStyle(
-                              fontFamily: "PoppinsSemiBold",
-                              fontSize: 20,
-                              color: Colors.white),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text("Fill out your medical form!",
-                            style:
-                                TextStyle(fontSize: 14, color: Colors.white)),
-                        SizedBox(
-                          height: 15,
-                        ),
                         Container(
-                          padding: EdgeInsets.all(12),
                           decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Get started",
-                              style: TextStyle(
-                                  fontFamily: "PoppinsMedium",
-                                  fontSize: 15,
-                                  color: Colors.white),
+                            border: Border(
+                              bottom: BorderSide(color: Colors.grey),
                             ),
                           ),
-                        )
+                          child: Text(
+                            "Dr. C Anderson",
+                            style: TextStyle(
+                              fontFamily: "PoppinsBold",
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                              overflow: TextOverflow.ellipsis
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  padding: EdgeInsets.only(bottom: 2),
+                                  child: Text(
+                                    "Cardiologists ",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontFamily: "PoppinsSemiBold"),
+                                  )),
+                              Text(
+                                "The Valley Hospital",
+                                style: TextStyle(
+                                    fontSize: 11,
+                                    fontFamily: "PoppinsSemiBoldItalic"),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(Icons.star, size: 20, ),
+                            Container(
+                                padding: EdgeInsets.symmetric(horizontal: 5),
+                                child: Text("4.8")),
+                          ],
+                        ),
                       ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-
-          SizedBox(
-            height: 5,
-          ),
-
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search),
-                  border: InputBorder.none,
-                  hintText: "How can we help you?",
+                    )
+                  ],
                 ),
               ),
-            ),
-          ),
-
-          SizedBox(
-            height: 15,
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              height: 80,
-              child: ListView(
-                children: [
-                  Container(
-                    child: Row(
-                      children: [Text("Dentist")],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     ));
   }
