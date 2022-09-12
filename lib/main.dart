b215b1a01e068b7671fc20e21fa4362fcd5ff7cb
doanyennhi <s3880599@rmit.edu.vn>
2022-09-13 02:19:15 +1000

    Add doctor appointment screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nd_telemedicine_app/screens/chat_menu.dart';
 import 'package:nd_telemedicine_app/screens/forgot_password.dart';
 import 'package:nd_telemedicine_app/screens/home.dart';
+import 'package:nd_telemedicine_app/screens/list_doctor.dart';
 import 'package:nd_telemedicine_app/screens/medical_info.dart';
 import 'package:nd_telemedicine_app/screens/personal_info.dart';
 import 'package:nd_telemedicine_app/screens/sign_in.dart';
@@ -50,7 +51,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: buildMaterialColor(const Color(0xff2B8D78)),
         fontFamily: 'Poppins',
       ),
-      home: const ProfileScreen(),
+      home: const PrescriptionScreen(),
     );
   }
 }
---
+++ lib/screens/list_doctor.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:nd_telemedicine_app/widgets/features/page_title.dart';
+
+class DoctorAppointmentScreen extends StatefulWidget {
+  const DoctorAppointmentScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DoctorAppointmentScreen> createState() => _DoctorAppointmentScreenState();
+}
+
+class _DoctorAppointmentScreenState extends State<DoctorAppointmentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              children: [
+                SizedBox(height: 20,),
+                PageTitle(title: "Appointment"),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Material(
+                      elevation: 10.0,
+                      shadowColor: Colors.black45,
+                      borderRadius: const BorderRadius.all(Radius.circular(40)),
+                      child: TextField(
+                        cursorColor: Colors.grey,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: 'Find doctor...',
+                          hintStyle:
+                          const TextStyle(color: Colors.grey, fontSize: 15),
+                          prefixIcon: const Icon(Icons.search, color: Color(0xff38B69A),),
+                          suffixIcon: Icon(Icons.filter_list_rounded, color: Color(0xff38B69A),),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          )
+        ),
+      );
+  }
+}
