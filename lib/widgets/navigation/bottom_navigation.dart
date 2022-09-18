39ddf5ec741a87ecbb308e48bca4895ab5903be9
doanyennhi <s3880599@rmit.edu.vn>
2022-09-19 04:32:08 +1000

    Create bottom nav component

---
+++ lib/widgets/navigation/bottom_navigation.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today_outlined),
+          label: 'Appointment',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.note_add_outlined),
+          label: 'Prescription',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat_bubble_outline_rounded),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline_rounded),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
