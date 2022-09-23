5190805c3f5bdcc6d8f4c603c34b61879c013de2
doanyennhi <s3880599@rmit.edu.vn>
2022-09-24 00:48:02 +1000

    Add showing a day's agenda or appointment, style agenda

--- lib/screens/doctor_schedule.dart
+++ lib/screens/doctor_schedule.dart
@@ -39,6 +39,26 @@ class TimeslotDataSource extends CalendarDataSource {
 
 class _DoctorScheduleState extends State<DoctorSchedule> {
 
+  List<TimeSlot> _getDataSource() {
+    final List<TimeSlot> slots = <TimeSlot>[];
+    final DateTime today = DateTime.now();
+
+    final DateTime startTime =
+    DateTime(today.year, today.month, today.day + 1, 9, 0, 0);
+    final DateTime start2 =
+    DateTime(today.year, today.month, today.day + 1, 13, 0, 0);
+    final DateTime start3 =
+    DateTime(today.year, today.month, today.day + 1, 15, 0, 0);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+    slots.add(TimeSlot(
+        'Busy', startTime, endTime, const Color(0xFFBE3050)));
+    slots.add(TimeSlot(
+        'Busy', start2, start2.add(const Duration(hours: 1)), const Color(0xFFBE3050)));
+    slots.add(TimeSlot(
+        'Patient Baek Dohwa', start3, start3.add(const Duration(minutes: 30)), const Color(0xFF2B8D78)));
+    return slots;
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -69,7 +89,22 @@ class _DoctorScheduleState extends State<DoctorSchedule> {
                         ),
                         monthViewSettings: MonthViewSettings(
                             showAgenda: true,
+                            agendaItemHeight: 60,
+                            agendaStyle: AgendaStyle(
+                              appointmentTextStyle: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: "PoppinsRegular",),
+                              dateTextStyle: TextStyle(
+                                  color: Color(0xff031011),
+                                  fontSize: 20,
+                                  fontFamily: "PoppinsItalic"),
+                              dayTextStyle: TextStyle(
+                                  color: Color(0xff031011),
+                                  fontSize: 20,
+                                  fontFamily: "PoppinsSemiBold",),
+                            ),
                         ),
+                        dataSource: TimeslotDataSource(_getDataSource()),
               ),
             )
           ),
