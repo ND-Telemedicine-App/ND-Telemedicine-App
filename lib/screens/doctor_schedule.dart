72165d3e9d4ff0dfa874f5d6e1b957b60e4d818f
MingkaiWang-s3773115 <S3773115@student.rmit.edu.au>
2022-09-30 22:12:59 +1000

    fetch doctor's busy time to display in the calendar

--- lib/screens/doctor_schedule.dart
+++ lib/screens/doctor_schedule.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:nd_telemedicine_app/models/appointment.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:http/http.dart' as http;
 
-import '../services/models/busy_time_model.dart';
+import '../models/busyTime.dart';
 
 class DoctorSchedule extends StatefulWidget {
   const DoctorSchedule({Key? key}) : super(key: key);
@@ -41,149 +42,189 @@ class TimeslotDataSource extends CalendarDataSource {
   }
 }
 
-class _DoctorScheduleState extends State<DoctorSchedule> {
-  // get busyTimeLists of a doctor
-  List<TimeSlot> _getDataSource() {
-    final List<TimeSlot> slots = <TimeSlot>[];
-    final DateTime today = DateTime.now();
-    final List<BusyTime> busyTimes = <BusyTime>[];
+//fetch busy time data from database using API
+Future<List<BusyTime>> getBusyTimes(http.Client client, int doctorId)async{
+  final response = await client.get(Uri.parse("http://10.0.2.2:8080/busyTime/$doctorId"));
 
-    final dateFormat = DateFormat("hh:mm");
+  return compute(parseBusyTime, response.body);
+}
 
-    for(var i=0; i<busyTimes.length; i++){
-      DateTime startTime = dateFormat.parse(busyTimes[i].getBusyTime() ?? "");
-      slots.add(TimeSlot('Busy',
-          startTime,
-          startTime.add(Duration(hours: busyTimes[i].getDuration() ?? 0)),
-          const Color(0xFFEFCCD4)));
-    }
+List<BusyTime> parseBusyTime(String response){
+  final parsed = jsonDecode(response).cast<Map<String, dynamic>>();
 
-    final DateTime start2 =
-    DateTime(today.year, today.month, today.day + 1, 13, 0, 0);
-    final DateTime start3 =
-    DateTime(today.year, today.month, today.day + 1, 15, 0, 0);
-    // final DateTime endTime = startTime.add(const Duration(hours: 2));
-    // slots.add(TimeSlot('Busy', startTime, endTime, const Color(0xFFEFCCD4)));
-    slots.add(TimeSlot('Busy', start2, start2.add(const Duration(hours: 1)),
+  return parsed
+      .map<BusyTime>((json) => BusyTime.fromJson(json))
+      .toList();
+}
+
+//convert to Timeslot data source
+Future<TimeslotDataSource> _getDataSource() async {
+  final List<TimeSlot> slots = <TimeSlot>[];
+  final DateTime today = DateTime.now();
+
+  var busyTimes = await getBusyTimes(http.Client(), 1);
+
+  final dateFormat = DateFormat("dd-MM-yy HH:mm");
+
+  for(var i=0; i<busyTimes.length; i++){
+    DateTime startTime = dateFormat.parse(busyTimes[i].getStartTime());
+    slots.add(TimeSlot('Busy',
+        startTime,
+        startTime.add(Duration(hours: busyTimes[i].getDuration())),
         const Color(0xFFEFCCD4)));
-    slots.add(TimeSlot('Patient Baek Dohwa', start3,
-        start3.add(const Duration(minutes: 30)), const Color(0xFF78CEBB)));
-    return slots;
   }
 
-  Future<List<BusyTime>> getBusyTimes(var doctorId)async{
-    var data = await http.get(Uri.parse("http://localhost:8080/busyTime/$doctorId"));
-    var jsonData = json.decode(data.body);
+  // final DateTime start2 =
+  // DateTime(today.year, today.month, today.day + 1, 13, 0, 0);
+  final DateTime start3 =
+  DateTime(today.year, today.month, today.day + 1, 15, 0, 0);
+  // final DateTime endTime = startTime.add(const Duration(hours: 2));
+  // slots.add(TimeSlot('Busy', startTime, endTime, const Color(0xFFEFCCD4)));
+  // slots.add(TimeSlot('Busy', start2, start2.add(const Duration(hours: 1)),
+  //     const Color(0xFFEFCCD4)));
+  slots.add(TimeSlot('Patient Baek Dohwa', start3,
+      start3.add(const Duration(minutes: 30)), const Color(0xFF78CEBB)));
+  return TimeslotDataSource(slots);
+}
 
-    List<BusyTime> busyTimes = [];
-    for(var time in jsonData){
-      BusyTime busyTime = new BusyTime();
-      busyTime.id = time["id"];
-      busyTime.doctorId = time["doctorId"];
-      busyTime.busyTime = time["busyTime"];
-      busyTime.duration = time["duration"];
-      busyTimes.add(busyTime);
-    }
-    return busyTimes;
-  }
-
+class _DoctorScheduleState extends State<DoctorSchedule>{
+  late Future<TimeslotDataSource> timeslotDataSource;
+  // get busyTimeLists of a doctor
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double heightWidth = MediaQuery.of(context).size.height;
-
-    _showAddDialog() async {
-      showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('Add Busy Time', style: TextStyle(
-            fontFamily: "PoppinsSemiBold",
-          ),),
-          content: Text("Choose a time"),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: Text('Cancel', style: TextStyle(
-                  fontFamily: "PoppinsMedium",
-              fontSize: 16),),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Save'),
-              child: const Text('Save', style: TextStyle(
-                  fontFamily: "PoppinsMedium",
-                  fontSize: 16),),
-            ),
-          ],
-        ),
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Color(0xffFDFFFE),
-        elevation: 0,
-        title: Text(
-          "Appointments",
-          style: const TextStyle(
-              fontFamily: "PoppinsBold",
-              color: Color(0xff2B8D78),
-              fontSize: 30),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xff2B8D78),
-        onPressed: _showAddDialog,
-        child: Icon(Icons.add),
-      ),
-      body: SafeArea(
-        child: Padding(
-            padding: EdgeInsets.only(
-                top: heightWidth * 0.05,
-                left: screenWidth * 0.05,
-                right: screenWidth * 0.05),
-            child: Center(
-              child: SfCalendar(
-                view: CalendarView.month,
-                showNavigationArrow: true,
-                showDatePickerButton: true,
-                //cellBorderColor:  Color(0xff38b69a),
-                appointmentTextStyle: TextStyle(fontFamily: "PoppinsRegular"),
-                headerStyle: CalendarHeaderStyle(
-                    textStyle: TextStyle(
-                  color: Color(0xff2B8D78),
-                  fontSize: 18,
-                  fontFamily: "PoppinsSemiBold",
-                )),
-                viewHeaderStyle: ViewHeaderStyle(
-                  backgroundColor: Color(0xffddfff8),
-                  dayTextStyle: TextStyle(
-                      color: Color(0xff031011), fontFamily: "PoppinsMedium"),
-                ),
-                monthViewSettings: MonthViewSettings(
-                  showAgenda: true,
-                  agendaItemHeight: 60,
-                  agendaStyle: AgendaStyle(
-                    appointmentTextStyle: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xff031011),
-                      fontFamily: "PoppinsRegular",
-                    ),
-                    dateTextStyle: TextStyle(
-                        color: Color(0xff031011),
-                        fontSize: 20,
-                        fontFamily: "PoppinsItalic"),
-                    dayTextStyle: TextStyle(
-                      color: Color(0xff031011),
-                      fontSize: 20,
-                      fontFamily: "PoppinsSemiBold",
-                    ),
-                  ),
-                ),
-                dataSource: TimeslotDataSource(_getDataSource()),
-              ),
-            )),
-      ),
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Doctor's busy time: "),
+          ),
+          body: FutureBuilder <TimeslotDataSource>(
+            future: _getDataSource(),
+            builder: (context, snapshot){
+              if(snapshot.connectionState == ConnectionState.done){
+                if(snapshot.hasError) {
+                  return Center(
+                      child: Text("${snapshot.error} occurred!",
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 20),
+                      )
+                    );
+                  }else if(snapshot.hasData){
+                  return DoctorSchedulePage(timeslotDataSource: snapshot.data!);
+                }
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+                },
+          ),
+        )
     );
   }
+}
+
+class DoctorSchedulePage extends StatelessWidget{
+  const DoctorSchedulePage({Key? key, required this.timeslotDataSource}):super(key: key);
+
+  final TimeslotDataSource timeslotDataSource;
+    @override
+    Widget build(BuildContext context) {
+      double screenWidth = MediaQuery.of(context).size.width;
+      double heightWidth = MediaQuery.of(context).size.height;
+
+      _showAddDialog() async {
+        showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Add Busy Time', style: TextStyle(
+              fontFamily: "PoppinsSemiBold",
+            ),),
+            content: Text("Choose a time"),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: Text('Cancel', style: TextStyle(
+                    fontFamily: "PoppinsMedium",
+                    fontSize: 16),),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Save'),
+                child: const Text('Save', style: TextStyle(
+                    fontFamily: "PoppinsMedium",
+                    fontSize: 16),),
+              ),
+            ],
+          ),
+        );
+      }
+
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Color(0xffFDFFFE),
+            elevation: 0,
+            title: Text(
+              "Appointments",
+              style: const TextStyle(
+                  fontFamily: "PoppinsBold",
+                  color: Color(0xff2B8D78),
+                  fontSize: 30),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Color(0xff2B8D78),
+            onPressed: _showAddDialog,
+            child: Icon(Icons.add),
+          ),
+          body: SafeArea(
+            child: Padding(
+                padding: EdgeInsets.only(
+                    top: heightWidth * 0.05,
+                    left: screenWidth * 0.05,
+                    right: screenWidth * 0.05),
+                child: Center(
+                  child: SfCalendar(
+                    view: CalendarView.month,
+                    showNavigationArrow: true,
+                    showDatePickerButton: true,
+                    //cellBorderColor:  Color(0xff38b69a),
+                    appointmentTextStyle: TextStyle(fontFamily: "PoppinsRegular"),
+                    headerStyle: CalendarHeaderStyle(
+                        textStyle: TextStyle(
+                          color: Color(0xff2B8D78),
+                          fontSize: 18,
+                          fontFamily: "PoppinsSemiBold",
+                        )),
+                    viewHeaderStyle: ViewHeaderStyle(
+                      backgroundColor: Color(0xffddfff8),
+                      dayTextStyle: TextStyle(
+                          color: Color(0xff031011), fontFamily: "PoppinsMedium"),
+                    ),
+                    monthViewSettings: MonthViewSettings(
+                      showAgenda: true,
+                      agendaItemHeight: 60,
+                      agendaStyle: AgendaStyle(
+                        appointmentTextStyle: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xff031011),
+                          fontFamily: "PoppinsRegular",
+                        ),
+                        dateTextStyle: TextStyle(
+                            color: Color(0xff031011),
+                            fontSize: 20,
+                            fontFamily: "PoppinsItalic"),
+                        dayTextStyle: TextStyle(
+                          color: Color(0xff031011),
+                          fontSize: 20,
+                          fontFamily: "PoppinsSemiBold",
+                        ),
+                      ),
+                    ),
+                    dataSource: timeslotDataSource,
+                  ),
+                )),
+          ),
+        );
+  }
+
 }
--- lib/services/models/busy_time_model.dart
+++ lib/services/models/busy_time_model.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'dart:convert';
 
