220f6a4d4c414af581674d1be582aaaaa2a2de32
doanyennhi <s3880599@rmit.edu.vn>
2022-10-09 00:41:06 +1100

    Add info card for patients and appointments

--- lib/screens/admin/dashboard.dart
+++ lib/screens/admin/dashboard.dart
@@ -20,98 +20,256 @@ class _DashboardScreenState extends State<DashboardScreen> {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-        body: SafeArea(
-      child: Center(
-        child: Column(
-          children: [
-            PageTitle(title: "Dashboard"),
-            Text(
-              "You can view the clinic's data here",
-              style: TextStyle(
-                color: Color(0xff38b69a),
-                fontFamily: "PoppinsMedium",
-                fontSize: 16,
+        body: SingleChildScrollView(
+      child: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              PageTitle(title: "Dashboard"),
+              Text(
+                "You can view the clinic's data here",
+                style: TextStyle(
+                  color: Color(0xff38b69a),
+                  fontFamily: "PoppinsMedium",
+                  fontSize: 16,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            FutureBuilder<List>(
-              future: getData(doctorUri),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DoctorDataScreen(data: snapshot.data!,
-                            ),
-                          ));
-                    },
-                    child: Column(
-                      children: [
-                        Material(
-                          borderRadius: BorderRadius.circular(20),
-                          elevation: 10,
-                          child: Container(
-                            width: screenWidth * 0.7,
-                            padding: EdgeInsets.all(10),
-                            //margin: EdgeInsets.only(bottom: 40),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Color(0xffddfff8),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.1),
-                                  blurRadius: 40,
-                                  offset: Offset(
-                                      0.5, 1), // changes position of shadow
-                                ),
-                              ],
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: <Widget>[
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.transparent,
-                                    shape: BoxShape.circle,
-                                    border: Border.all(
-                                      color: Color(0xff2b8d78),
-                                      width: 10,
+              SizedBox(
+                height: 30,
+              ),
+              FutureBuilder<List>(
+                future: getData(doctorUri),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DoctorDataScreen(data: snapshot.data!,
+                              ),
+                            ));
+                      },
+                      child: Column(
+                        children: [
+                          Material(
+                            borderRadius: BorderRadius.circular(20),
+                            elevation: 10,
+                            child: Container(
+                              width: screenWidth * 0.7,
+                              padding: EdgeInsets.symmetric(vertical: 5),
+                              //margin: EdgeInsets.only(bottom: 40),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Color(0xffddfff8),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.1),
+                                    blurRadius: 40,
+                                    offset: Offset(
+                                        0.5, 1), // changes position of shadow
+                                  ),
+                                ],
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: <Widget>[
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.transparent,
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color: Color(0xff2b8d78),
+                                        width: 10,
+                                      ),
+                                    ),
+                                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+                                    child: Text(
+                                      snapshot.data!.length.toString(),
+                                      style: TextStyle(
+                                          fontFamily: "PoppinsBold", fontSize: 40),
                                     ),
                                   ),
-                                  padding: EdgeInsets.all(30),
-                                  child: Text(
-                                    snapshot.data!.length.toString(),
+                                  Text(
+                                    "Doctors",
                                     style: TextStyle(
-                                        fontFamily: "PoppinsBold", fontSize: 40),
-                                  ),
-                                ),
-                                Text(
-                                  "Doctors",
-                                  style: TextStyle(
-                                    fontFamily: "PoppinsMedium",
-                                    fontSize: 25,
-                                  ),
-                                )
-                              ],
+                                      fontFamily: "PoppinsMedium",
+                                      fontSize: 25,
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 30,
+                          SizedBox(
+                            height: 30,
+                          )
+                        ],
+                      )
+                    );
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+              FutureBuilder<List>(
+                future: getData(patientUri),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DoctorDataScreen(data: snapshot.data!,
+                                ),
+                              ));
+                        },
+                        child: Column(
+                          children: [
+                            Material(
+                              borderRadius: BorderRadius.circular(20),
+                              elevation: 10,
+                              child: Container(
+                                width: screenWidth * 0.7,
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                //margin: EdgeInsets.only(bottom: 40),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Color(0xff38b69a),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.1),
+                                      blurRadius: 40,
+                                      offset: Offset(
+                                          0.5, 1), // changes position of shadow
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: <Widget>[
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.transparent,
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: Color(0xff031011),
+                                          width: 10,
+                                        ),
+                                      ),
+                                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+                                      child: Text(
+                                        snapshot.data!.length.toString(),
+                                        style: TextStyle(
+                                            fontFamily: "PoppinsBold",
+                                            fontSize: 40,
+                                            color: Color(0xfffdfffe),),
+                                      ),
+                                    ),
+                                    Text(
+                                      "Patients",
+                                      style: TextStyle(
+                                        fontFamily: "PoppinsMedium",
+                                        color: Color(0xfffdfffe),
+                                        fontSize: 25,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            )
+                          ],
                         )
-                      ],
-                    )
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
-            ),
-          ],
+                    );
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+              FutureBuilder<List>(
+                future: getData(appointmentUri),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DoctorDataScreen(data: snapshot.data!,
+                                ),
+                              ));
+                        },
+                        child: Column(
+                          children: [
+                            Material(
+                              borderRadius: BorderRadius.circular(20),
+                              elevation: 10,
+                              child: Container(
+                                width: screenWidth * 0.7,
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                //margin: EdgeInsets.only(bottom: 40),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Color(0xffbe3050),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.1),
+                                      blurRadius: 40,
+                                      offset: Offset(
+                                          0.5, 1), // changes position of shadow
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: <Widget>[
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.transparent,
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: Color(0xffddfff8),
+                                          width: 10,
+                                        ),
+                                      ),
+                                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+                                      child: Text(
+                                        snapshot.data!.length.toString(),
+                                        style: TextStyle(
+                                          fontFamily: "PoppinsBold",
+                                          fontSize: 40,
+                                          color: Color(0xfffdfffe),),
+                                      ),
+                                    ),
+                                    Text(
+                                      "Appointments",
+                                      style: TextStyle(
+                                        fontFamily: "PoppinsMedium",
+                                        color: Color(0xfffdfffe),
+                                        fontSize: 25,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            )
+                          ],
+                        )
+                    );
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     ));
