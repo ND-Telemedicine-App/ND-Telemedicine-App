72e89fcb7cd6d07f9cac84a3705ca65cc9c4f260
doanyennhi <s3880599@rmit.edu.vn>
2022-10-08 16:32:48 +1100

    Add columns and cells

--- lib/screens/admin/data_screen.dart
+++ lib/screens/admin/data_screen.dart
@@ -15,31 +15,113 @@ class _DoctorDataScreenState extends State<DoctorDataScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(
-          color: Colors.white,
-          onPressed: () {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => DashboardScreen()));
-          },
+        appBar: AppBar(
+          leading: BackButton(
+            color: Colors.white,
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => DashboardScreen()));
+            },
+          ),
+          centerTitle: true,
+          title: Text(
+            "Patients",
+            style: const TextStyle(fontFamily: "PoppinsBold"),
+          ),
         ),
-        centerTitle: true,
-        title: Text(
-          "Patients",
-          style: const TextStyle(fontFamily: "PoppinsBold"),
-        ),
-      ),
         body: SafeArea(
-        child: Center(
-        child: Column(
-          children: [
-            Text(widget.data.length.toString())
-          ],
-        )
-    ),
-    ),
-    );
+          child: Center(
+            child: DataTable(columns: const [
+              DataColumn(
+                label: Text(
+                  'Email',
+                  style: TextStyle(
+                    color: Color(0xff2b8d78),
+                    fontSize: 18.0,
+                  ),
+                ),
+                numeric: false,
+              ),
+              DataColumn(
+                label: Text(
+                  'Full Name',
+                  style: TextStyle(
+                    color: Color(0xff2b8d78),
+                    fontSize: 18.0,
+                  ),
+                ),
+                numeric: false,
+              ),
+              DataColumn(
+                label: Text(
+                  'Address',
+                  style: TextStyle(
+                    color: Color(0xff2b8d78),
+                    fontSize: 18.0,
+                  ),
+                ),
+                numeric: false,
+              ),
+            ], rows: <DataRow>[
+              ...widget.data.map(
+                (info) => DataRow(cells: [
+                  DataCell(
+                    Container(
+                      width: 100,
+                      child: Text(
+                        info["email"],
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  DataCell(
+                    Container(
+                      width: 60.0,
+                      child: Center(
+                        child: Text(
+                          info["fullName"],
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  DataCell(
+                    Container(
+                      width: 60.0,
+                      child: Center(
+                        child: Text(
+                          info["address"],
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+              )
+            ]
+                //             rows: widget.data
+                //             .map((info) => DataRow(
+                //               cells: [
+                //               DataCell(
+                //               Container(
+                //               width: 100,
+                //               child: Text(
+                //                 info["email"],
+                //                 softWrap: true,
+                //                 overflow: TextOverflow.ellipsis,
+                //                 style: TextStyle(fontWeight: FontWeight.w600),
+                //               ),
+                //             ),
+                //             ),
+                //       ]
+                // )
+                //             )
+                ),
+          ),
+        ));
   }
 }
